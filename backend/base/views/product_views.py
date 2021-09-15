1ea3f11a0d9492de146cabc74a35a50560504d86
MithraR <ms12292@nyu.edu>
2021-09-15 15:55:47 -0400

    Initial Commit

--- backend/base/views/product_views.py
+++ backend/base/views/product_views.py
@@ -1,17 +1,11 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework import serializers
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
-from .products import products
-from .models import Product
-from .serializers import ProductSerializer
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from base.models import Product
+from base.serializers import ProductSerializer
 
-
-# Create your views here.
-@api_view(['GET'])
-def getRoutes(request):
-    return Response('Hello')
+from rest_framework import status
 
 @api_view(['GET'])
 def getProducts(request):
@@ -19,9 +13,10 @@ def getProducts(request):
     serializer = ProductSerializer(products,many=True)
     return Response(serializer.data)
 
+
+
 @api_view(['GET'])
 def getProduct(request,pk):
     product = Product.objects.get(_id=pk)
     serializer = ProductSerializer(product,many=False)
     return Response(serializer.data)
-
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,17 @@ import Header1 from './components/Header1'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
 import CartScreen from './screens/CartScreen'
+import LoginScreen from './screens/LoginScreen'
+import RegisterScreen from './screens/RegisterScreen'
+import ProfileScreen from './screens/ProfileScreen'
+import ShippingScreen from './screens/ShippingScreen'
+import PaymentScreen from './screens/PaymentScreen'
+import PlaceOrderScreen from './screens/PlaceOrderScreen'
+import OrderScreen from './screens/OrderScreen'
+
+
+
+
 function App() {
   return (
     <Router>
@@ -12,8 +23,16 @@ function App() {
         <Header1 />
         <Container>
             <Route path='/' component={HomeScreen} exact />
+            <Route path='/login' component={LoginScreen} />
+            <Route path='/register' component={RegisterScreen} />
+            <Route path='/profile' component={ProfileScreen} />
             <Route path='/product/:id' component={ProductScreen}  />
             <Route path='/cart/:id?' component={CartScreen}  />
+            <Route path='/shipping' component={ShippingScreen} />
+            <Route path='/payment' component={PaymentScreen} />
+            <Route path='/placeorder' component={PlaceOrderScreen} />
+            <Route path='/order/:id' component={OrderScreen} />
+
         </Container>        
       </main>
       <Footer />
